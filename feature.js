9f207bad7510d48ee2afcbf4bed67cfbc97d45b3
Chase Farmer <cfarmer@fanhattan.com>
2016-12-04 16:48:01 -0800

    fix canvascapture

--- feature.js
+++ feature.js
@@ -167,7 +167,7 @@
     })(util.create("canvas")),
 
     // Test if Canvas Capture is supported
-    canvasCapture: (CanvasCaptureMediaStreamTrack in window),
+    canvasCapture: (window.CanvasCaptureMediaStreamTrack),
 
     // Test if SVG is supported
     svg : !!document.createElementNS && !!document.createElementNS("http://www.w3.org/2000/svg", "svg").createSVGRect,
